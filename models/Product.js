c61ec1ef92d2a7f9c4baa75a258012ff0875351f
Mr WhiteKidd <austinslater58@gmail.com>
2022-02-26 21:37:20 -0600

    added all routes to category-routes product-routes tag-routes

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     // define columns
+    product_name: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    inventory: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      Validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
